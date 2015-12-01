77ab868501220e2d1a2713bd6924d9c366d09eb0
sindrisverris <sindri15@ru.is>
2015-12-01 16:37:04 +0000

    Update ui.cpp

--- code/ui.cpp
+++ code/ui.cpp
@@ -10,3 +10,21 @@ string ui::getInput(){
     return input;
 
 }
+string ui::getHelp(){
+    string help;
+    ui::getHelp cmd;
+    getHelp(cin, help);
+    command >> cmd;
+    if(cmd == 'help'||'HELP' || 'Help' || '?')
+    {
+            cout<< "The commands are: " << endl;
+            cout<< "add: To add information"<< endl;
+            cout<< "display: display information on screen"<< endl;
+            cout<< "sort: sort the list"<< endl;
+            cout<< "search: Type in a word to search the list"<< endl;
+    }
+    
+    else if(cmd == 'add')
+    
+    
+    }
