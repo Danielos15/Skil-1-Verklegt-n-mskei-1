f26819c516f1207e481ec8b525e6ab3aa79bc236
arnarb94 <arnarb94@gmail.com>
2015-12-08 20:28:51 +0000

    Update service.cpp

--- code/service.cpp
+++ code/service.cpp
@@ -216,7 +216,7 @@ void service::addScientist(){
     }else {
         interface.renderText("What year did " + gender + " die: ");
         death = interface.getInt();
-        while (death < born && death > 2015){
+        while (death < born || death > 2015){
             interface.renderText(gender + " can't have died that year \n");
             interface.renderText("try another year: ");
             death = interface.getInt();
